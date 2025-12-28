0685caaf15ecc5209edd26fa4955fce96edc2376
Ahmed Beheiri <abeheiri@getment.io>
2025-12-29 00:01:47 +0200

    refactor: streamline backend URL protocol determination using a ternary operator.

--- apps/web/lib/client.dart
+++ apps/web/lib/client.dart
@@ -4,10 +4,11 @@ Client? _client;
 
 Client get client {
   // Use 'BACKEND_URL' if provided (e.g. from --dart-define), otherwise default to localhost
-  var baseUrl = String.fromEnvironment('BACKEND_URL', defaultValue: 'http://127.0.0.1:8080/');
-  if (!baseUrl.startsWith('http')) {
-    baseUrl = 'https://$baseUrl';
-  }
+  const envUrl = String.fromEnvironment('BACKEND_URL', defaultValue: 'http://127.0.0.1:8080/');
+  
+  // Ensure protocol is present
+  final baseUrl = envUrl.startsWith('http') ? envUrl : 'https://$envUrl';
+  
   _client ??= Client(baseUrl);
   return _client!;
 }
